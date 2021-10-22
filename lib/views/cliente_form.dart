16d01b0d862fb7cc494e6c0baa93ff386117287d
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2021-10-22 13:48:17 -0300

    Cliente Form

--- lib/views/cliente_form.dart
+++ lib/views/cliente_form.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:medicao/models/cliente_model.dart';
+import 'package:medicao/provider/provider_clientes.dart';
+import 'package:provider/provider.dart';
 
 class ClienteForm extends StatelessWidget {
   final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,10 +17,22 @@ class ClienteForm extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.save),
             onPressed: () {
-              // TODO: Fazer validação do form
-              final isValid = _form.currentState.validate();
+              // TODO Fazer validação do form
+              final isValid = _form.currentState!.validate();
               if (isValid) {
-                _form.currentState.save();
+                _form.currentState!.save();
+                Provider.of<Clientes>(context, listen: false).put(Cliente(
+                  id: _formData['id'] != null
+                      ? int.parse(_formData['id']!)
+                      : -1,
+                  nome: _formData['nome']!,
+                  cep: _formData['cep']!,
+                  logradouro: _formData['logradouro']!,
+                  numero: _formData['numero']!,
+                  bairro: _formData['bairro']!,
+                  cidade: _formData['cidade']!,
+                  estado: _formData['estado']!,
+                ));
                 Navigator.of(context).pop();
               }
             },
@@ -29,49 +46,61 @@ class ClienteForm extends StatelessWidget {
           child: Column(
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'Nome'),
+                onSaved: (value) => _formData['nome'] = value!,
                 validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Nome inválido';
-                  }
-                  if (value.trim().length < 3) {
-                    return 'Nome muito curto';
-                  }
-                  return null;
+                  return value == null ||
+                          value.trim().isEmpty ||
+                          value.trim().length < 3
+                      ? 'Nome inválido ou muito curto'
+                      : null;
                 },
-                onSaved: (value) {
-                  print(value);
-                },
+                decoration: InputDecoration(labelText: 'Nome'),
               ),
               TextFormField(
+                onSaved: (value) => _formData['cep'] = value!,
+                validator: (value) {
+                  return value!.trim().isEmpty || value.length < 8
+                      ? 'CEP precisa de 8 digitos'
+                      : null;
+                },
                 decoration: InputDecoration(
                   labelText: "CEP",
                   suffixIcon: IconButton(
                     icon: Icon(Icons.travel_explore),
                     onPressed: () {
-                      // FIXME:Realizar importação de dados de endereço
+                      // TODO Realizar importação de dados de endereço
                     },
                   ),
                 ),
-                validator: (value){
-                  if(value.trim().isEmpty || value.length<9){
-                    return 'CEP precisa de 9 digitos';
-                  }
-                  return null;
-                },
                 keyboardType: TextInputType.number,
                 inputFormatters: [
                   FilteringTextInputFormatter.digitsOnly,
-                  LengthLimitingTextInputFormatter(9),
+                  LengthLimitingTextInputFormatter(8),
                 ],
               ),
               TextFormField(
+                onSaved: (value) => _formData['logradouro'] = value!,
+                validator: (value) {
+                  return value!.trim().isEmpty || value.length < 4
+                      ? 'Endereço muito curto'
+                      : null;
+                },
                 decoration: InputDecoration(labelText: "Endereço"),
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: "nº"),
+                onSaved: (value) => _formData['numero'] = value!,
+                validator: (value) {
+                  return value!.trim().isEmpty ? 'Necessário informar' : null;
+                },
+                decoration: InputDecoration(labelText: "nº e complemento"),
               ),
               TextFormField(
+                onSaved: (value) => _formData['bairro'] = value!,
+                validator: (value) {
+                  return value!.trim().isEmpty || value.length < 4
+                      ? 'Bairro muito curto'
+                      : null;
+                },
                 decoration: InputDecoration(labelText: "Bairro"),
               ),
               Row(
@@ -79,13 +108,27 @@ class ClienteForm extends StatelessWidget {
                   Expanded(
                     flex: 3,
                     child: TextFormField(
+                      onSaved: (value) => _formData['cidade'] = value!,
+                      validator: (value) {
+                        return value!.length < 3
+                            ? 'Nome da cidade muito curto'
+                            : null;
+                      },
                       decoration: InputDecoration(labelText: "Cidade"),
                     ),
                   ),
                   VerticalDivider(),
                   Expanded(
                     child: TextFormField(
+                      onSaved: (value) =>
+                          _formData['estado'] = value!.toUpperCase(),
+                      validator: (value) {
+                        return value!.length != 2 ? 'Informe' : null;
+                      },
                       decoration: InputDecoration(labelText: "Estado"),
+                      keyboardType: TextInputType.name,
+                      inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                      textCapitalization: TextCapitalization.characters,
                     ),
                   ),
                 ],
