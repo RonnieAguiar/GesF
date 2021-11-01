8a5907797ffc46569adae9f386b7d66a3b5b3def
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2021-11-01 12:43:56 -0300

    conexão PHP PDO pgsql

---
+++ gesfapi/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$db_name = "";
+$db_server = "";
+$db_port = "";
+$db_user = "";
+$db_pass = "";
+
+$db = new PDO('pgsql:host={$db_server};port={$db_port};dbname={$db_name}', $db_user, $db_pass);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
